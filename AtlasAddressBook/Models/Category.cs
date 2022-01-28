424021673e8617eab30fcfe44cfb3c1527c89fc4
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-01-28 16:53:40 -0600

    Add project files.

---
+++ AtlasAddressBook/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AtlasAddressBook.Models
+{
+    public class Category
+    {
+        //primary Key
+        public int Id { get; set; }
+        //Foreighn Key
+        public string? UserId { get; set; }
+        [Required]
+        [Display(Name ="Category Name")]
+        public string? Name { get; set; }
+        //Navigation Properties (within db)
+        public virtual AppUser? User { get; set; }
+
+        public virtual ICollection<Contact> Contacts { get; set; } = new HashSet<Contact>();
+       
+    }
+}
---
+++ AtlasAddressBook/Models/Contact.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AtlasAddressBook.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+public string? UserId { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        [NotMapped]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+
+
+        [DataType(DataType.Date)]
+        public DateTime? Birthday { get; set; }
+        [Required]
+        public string? Address1 { get; set; }
+
+        public string? Address2 { get; set; }
+        [Required]
+        public string? City { get; set; }
+        [Required]
+        public string? State { get; set; }
+        [Required]
+        public string? ZipCode { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string? EmailAddress { get; set;}
+        [DataType(DataType.PhoneNumber)]
+        public string? PhoneNumber { get; set;}
+
+        [DataType(DataType.Date)]
+        public DateTime created { get; set; }
+
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        [Display(Name =  "Contact Image")]
+        public IFormFile? formFile { get; set; }
+        public byte[]? ImageData { get; set; }
+        public string? ImageType { get; set; }
+
+        public virtual AppUser? User { get; set; }
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
+    }
+}
