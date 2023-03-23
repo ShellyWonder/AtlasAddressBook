2152b6a4269e82e13f0bbc204ad2b9cf93dd42f2
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2023-03-23 08:48:01 -0500

    Ui modifications

--- AtlasAddressBook/wwwroot/js/site.js
+++ AtlasAddressBook/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function changeDemoUserButtonText() {
+    document.getElementById("demoUserBtn").innerHTML = "Try without Registering";
+}
+
+function changeNewUserButtonText() {
+    document.getElementById("newUserBtn").innerHTML = "CREATE NEW ACCOUNT";
+}
+
+function changeDemoUserBtnTxtBack() {
+    document.getElementById("demoUserBtn").innerHTML = "DEMO USER";
+}
+
+function changeNewUserBtnTxtBack() {
+    document.getElementById("newUserBtn").innerHTML = "NEW USER?";
+}
