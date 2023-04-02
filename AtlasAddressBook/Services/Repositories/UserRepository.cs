8c314aeec43352fc17ca8460673a0fd6407211c7
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2023-04-02 11:29:43 -0500

    Created User Repository

---
+++ AtlasAddressBook/Services/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using AtlasAddressBook.Data;
+using AtlasAddressBook.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AtlasAddressBook.Services.Repositories
+{
+    public class UserRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
+        public UserRepository(ApplicationDbContext context,
+                              UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<AppUser> GetAppUserByIdAsync(int userId)
+        {
+            AppUser appUser = await _userManager.FindByIdAsync(userId.ToString());
+
+            return appUser;
+        }
+    }
+}
